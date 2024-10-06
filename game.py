858737d42a52ee0dd6f756f1b1b484b72f823a35
twill732 <twdbronco18@gmail.com>
2024-10-06 18:07:52 -0400

    Update game.py

--- game.py
+++ game.py
@@ -4,14 +4,17 @@ This is the main file for the pygame project for the Programming 1 Lecture class
 Contributors (add your name to this list if you haven't):
 
 Gavin Conley
-
+Trevor Williams
 
 """
 import pygame
 
 pygame.init()
 
-white = (255, 255, 255)
+white = (255, 255, 255)   #lines
+dark_grey = (90, 90, 90)  #background
+red = (255, 0, 0)         #  X's
+green = (0, 255, 0)       #  O's
 
 resolution = (640, 480)
 screen = pygame.display.set_mode(resolution)
@@ -26,19 +29,21 @@ RING = 2
 
 board = [[EMPTY] * 5] * 5
 
-# Game's main loop
-while running:
+def game_loop(): # Game's main loop
+    running = True
+    while running:
 
-    # Go through pygame events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        # Go through pygame events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip()
+        clock.tick(60)
 
 
 pygame.quit()
+game_loop()
 #test comment
 print("This is a git test")
 
