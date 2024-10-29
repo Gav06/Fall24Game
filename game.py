738e1269a22033f68da28dcab1398bb4bf05fa6f
lucasallen1105 <lucasallen1105@gmail.com>
2024-10-29 16:28:31 -0400

    Added score

--- game.py
+++ game.py
@@ -13,10 +13,10 @@ import pygame
 
 width = 1280
 height = 720
+player_speed = 1
+player = pygame.Rect(width // 2 - 20 // 2, height // 2 - 20 // 2, 20, 20)
 
-
-
-screen = pygame.display.set_mode(width , height)
+screen = pygame.display.set_mode((width , height))
 pygame.display.set_caption("")
 
 
@@ -25,7 +25,24 @@ while running:
     for event in pygame.event.get():
         if pygame.event == pygame.QUIT:
             pygame.quit()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] and player.top > 0:
+        player.move_ip(0, -player_speed)
+    if keys[pygame.K_s] and player.bottom < 720:
+        player.move_ip(0, player_speed)
+    if keys[pygame.K_a] and player.left > 0:
+        player.move_ip(-player_speed, 0)
+    if keys[pygame.K_d] and player.right < 1280:
+        player.move_ip(player_speed, 0)
+    screen.fill((0,0,0))
+    pygame.draw.rect(screen,(255,255,255), player)
+    pygame.display.flip()
 
 pygame.init()
+
+
+
+
+
 
 pygame.quit()
