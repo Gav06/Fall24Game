5849ae5a42502e93d49f93aae1b0d7b2233559ca
twill732 <twdbronco18@gmail.com>
2024-10-29 16:29:12 -0400

    Update game.py

--- game.py
+++ game.py
@@ -8,24 +8,31 @@ Trevor Williams
 Lucas Allen
 
 """
-
 import pygame
+pygame.init()
 
 width = 1280
 height = 720
 
+black = (0,0,0)
+white = (255, 255, 255)
+font = pygame.font.Font(None, 100)
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Zombie Shooter")
 
 
-screen = pygame.display.set_mode(width , height)
-pygame.display.set_caption("")
-
+start_screen = "start"
+game_screen = "game"
+current_state =
 
 running = True
 while running:
     for event in pygame.event.get():
         if pygame.event == pygame.QUIT:
-            pygame.quit()
+            running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
 
-pygame.init()
+
 
 pygame.quit()
