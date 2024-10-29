bc1de65de8a43445447be372f74f9ec035f380b1
twill732 <twdbronco18@gmail.com>
2024-10-29 16:18:35 -0400

    Update game.py

--- game.py
+++ game.py
@@ -11,6 +11,21 @@ Lucas Allen
 
 import pygame
 
+width = 1280
+height = 720
+
+
+
+screen = pygame.display.set_mode(width , height)
+pygame.display.set_caption("")
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if pygame.event == pygame.QUIT:
+            pygame.quit()
+
 pygame.init()
 
 pygame.quit()
